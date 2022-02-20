f565c9d5e6092503be8f95ccf02c809af212d73a
YairVaqueroH <vyair3882@gmail.com>
2022-02-20 00:20:22 -0600

    MiRespaldo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+ import { Routes } from '@angular/router';
+
+ import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes=[
+  {
+  path: 'home',
+  component: HomeComponent
+},
+{
+  path: 'about',
+  component: AboutComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
+},
+{
+path: 'posts',
+loadChildren:() => import ('./pages/posts/posts.module').then(m => m.PostsModule)
+},
+{
+  path: '**',
+  redirectTo: 'home'
+}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes )
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+
+
+
+export class AppRoutingModule{}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import { HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    //AboutComponent,
+    //ContactComponent,
+    //HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
